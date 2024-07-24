68e9c084f7dfcd890069192e4db0c44b01e1328d
Prakhar Awasthi <pawasthi961@gmail.com>
2024-07-24 22:19:33 +0530

    created unary request response using grpc client

--- client/cmd/client/main.go
+++ client/cmd/client/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+
+	pb "github.com/pi-prakhar/go-grpc/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+
+	conn, err := grpc.NewClient("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NameList{
+	// 	Names: []string{"Akhil", "Alice", "Bob"},
+	// }
+
+	callSayHello(client)
+}
